cc986cb0385c036f2405c1c5b2f6fa2d99831d52
Githaiga <pythongeeko@gmail.com>
2024-03-04 16:27:30 +0300

    change to Brouser router

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import UserContextProvider from './contexts/CurrentUserContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <UserContextProvider>
         <App />
       </UserContextProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
