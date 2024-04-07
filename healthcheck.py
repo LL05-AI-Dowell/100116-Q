fe700875d8d05ab2e60f758a67698fd9795c5b4d
Manish <mdashsharma95@gmail.com>
2024-04-08 03:17:13 +0530

    Ft: Banckend V3

--- healthcheck.py
+++ healthcheck.py
@@ -9,4 +9,4 @@ from django.shortcuts import redirect
 @method_decorator(csrf_exempt, name='dispatch')
 class health_check(APIView):
     def get(self, request ):
-        return Response("Server is running ... , version : 2.0.1",status=status.HTTP_200_OK)
+        return Response("Server is running ... , version : 3.0.0",status=status.HTTP_200_OK)
